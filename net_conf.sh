ba88c19b6ac588a6b539c5115f27ec3ded50cd37
6Whj0gc <dwp1y3@gmail.com>
2025-12-02 20:12:26 +0800

    Add files via upload

---
+++ net_conf.sh
@@ -0,0 +1,17 @@
+echo '
+net.ipv4.tcp_rmem = 712500 712500 1425000
+net.ipv4.tcp_wmem = 712500 712500 1425000
+
+net.core.rmem_max = 1425000
+net.core.wmem_max = 1425000
+
+net.core.rmem_default = 712500
+net.core.wmem_default = 712500
+
+net.core.default_qdisc = fq
+net.ipv4.tcp_congestion_control=bbr
+
+' >> /etc/sysctl.conf
+
+sysctl -p
+
